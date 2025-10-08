69429d1fbd6c83c5ca891040dcb5f4272ec0a15e
denning01 <gichabadenning254@gmail.com>
2025-10-08 16:10:09 +0300

    inserts data to tables

--- answers.sql
+++ answers.sql
@@ -1,1 +1,25 @@
 
+### **Question 1 🧑‍🎓**  
+
+CREATE TABLE student (
+  id INT PRIMARY KEY,
+  fullName VARCHAR(100),
+  age INT
+);
+
+
+### **Question 2 ➕**  
+
+INSERT INTO student (id, fullName, age)
+VALUES
+  (1, 'Denning Gichaba', 24),
+  (2, 'Alice Mwangi', 22),
+  (3, 'Brian Otieno', 25);
+
+
+
+### **Question 3 🔄**  
+
+UPDATE student
+SET age = 20
+WHERE id = 2;
